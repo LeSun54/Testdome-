03b14fddc5e6be8448d7d198fb56a6697b8bad6a
Leon Sun <38485966+LeSun54@users.noreply.github.com>
2018-12-10 14:49:31 +0800

    Update File Owners.py

--- File Owners.py
+++ File Owners.py
@@ -1,4 +1,9 @@
-class FileOwners:
+#A palindrome is a word that reads the same backward or forward.
+
+#Write a function that checks if a given word is a palindrome. Character case should be ignored.
+
+#For example, is_palindrome("Deleveled") should return True as character case should be ignored, resulting in "deleveled", which is a palindrome since it reads the same backward and forward.class FileOwners:
+class Palindrome:
 
     @staticmethod
     def group_by_owners(files):
